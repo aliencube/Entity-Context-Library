1563bbbc854f03aacb58344aef02f9dcce55aeae
Justin Yoo <justin.yoo@aliencube.com>
2014-12-30 12:42:36 +1100

    Update build.bat

--- SourceCodes/build.bat
+++ SourceCodes/build.bat
@@ -12,7 +12,7 @@ IF NOT EXIST %MSBUILDDIR%msbuild.exe goto MissingMSBuildExe
 
 ::BUILD
 "tools\nuget.exe" restore EntityContextLibrary.sln
-"%MSBUILDDIR%msbuild.exe" "EntityContextLibrary\EntityContextLibrary.csproj" /t:ReBuild /p:Configuration=Release;TargetFrameworkVersion=v4.5;DefineConstants="TRACE;NET45";OutPutPath=bin\Release\net45\;DocumentationFile=bin\Release\net45\Aliencube.EntityContextLibrary.xml
+"%MSBUILDDIR%msbuild.exe" "03_Services\EntityContextLibrary\EntityContextLibrary.csproj" /t:ReBuild /p:Configuration=Release;TargetFrameworkVersion=v4.5;DefineConstants="TRACE;NET45";OutPutPath=bin\Release\net45\;DocumentationFile=bin\Release\net45\Aliencube.EntityContextLibrary.xml
 
 mkdir build
 del "build\*.nupkg"
@@ -21,7 +21,7 @@ del "build\*.nupkg"
 "tools\nuget.exe" setApiKey %1
 
 ::PACK
-"tools\nuget.exe" pack "EntityContextLibrary\EntityContextLibrary.nuspec" -OutputDirectory build
+"tools\nuget.exe" pack "03_Services\EntityContextLibrary\EntityContextLibrary.nuspec" -OutputDirectory build
 
 ::DEPLOY
 "tools\nuget.exe" push "build\*.nupkg"
