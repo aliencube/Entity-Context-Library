1e1c954faae8f97cd8d8c1104372ad14f192f1b9
Justin Yoo <justin.yoo@aliencube.com>
2015-10-19 12:15:14 +1100

    Update test methods for DbContextFactory

--- SourceCodes/Tests/EntityContextLibrary.Tests/DbContextFactoryTest.cs
+++ SourceCodes/Tests/EntityContextLibrary.Tests/DbContextFactoryTest.cs
@@ -1,15 +1,24 @@
 ﻿using System;
+
 using Aliencube.EntityContextLibrary.Interfaces;
+
 using FluentAssertions;
+
 using NUnit.Framework;
 
 namespace Aliencube.EntityContextLibrary.Tests
 {
+    /// <summary>
+    /// This represents the test entity for the <see cref="DbContextFactory{TContext}" /> class.
+    /// </summary>
     [TestFixture]
     public class DbContextFactoryTest
     {
         private IDbContextFactory _factory;
 
+        /// <summary>
+        /// Initialises all resources for tests.
+        /// </summary>
         [SetUp]
         public void Init()
         {
@@ -18,6 +27,9 @@ namespace Aliencube.EntityContextLibrary.Tests
             this._factory = new DbContextFactory<ProductContext>();
         }
 
+        /// <summary>
+        /// Release all resources after tests.
+        /// </summary>
         [TearDown]
         public void Cleanup()
         {
@@ -27,9 +39,15 @@ namespace Aliencube.EntityContextLibrary.Tests
             }
         }
 
+        /// <summary>
+        /// Tests whether the context factory returns DbContext with given type or not.
+        /// </summary>
+        /// <param name="expectedType">
+        /// The expected type.
+        /// </param>
         [Test]
         [TestCase(typeof(ProductContext))]
-        public void GetContext_GivenDetails_ReturnContext(Type expectedType)
+        public void ContextFactory_Should_Return_Context_Of_Given_Type(Type expectedType)
         {
             var context = this._factory.Context;
             context.Should().NotBeNull();
