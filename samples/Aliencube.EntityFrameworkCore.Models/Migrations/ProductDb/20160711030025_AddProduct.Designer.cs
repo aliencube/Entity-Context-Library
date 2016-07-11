8914c2581886f4a07571a199939fb236b28356aa
Justin Yoo <justin.yoo@aliencube.com>
2016-07-11 13:21:53 +1000

    Add Aliencube.EntityFrameworkCore.WebApp project

---
+++ samples/Aliencube.EntityFrameworkCore.Models/Migrations/ProductDb/20160711030025_AddProduct.Designer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Aliencube.EntityFrameworkCore.Models;
+
+namespace Aliencube.EntityFrameworkCore.Models.Migrations.ProductDb
+{
+    [DbContext(typeof(ProductDbContext))]
+    [Migration("20160711030025_AddProduct")]
+    partial class AddProduct
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Aliencube.EntityFrameworkCore.Models.Product", b =>
+                {
+                    b.Property<int>("ProductId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("ProductId")
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnName("Name")
+                        .HasAnnotation("MaxLength", 64);
+
+                    b.HasKey("ProductId");
+
+                    b.ToTable("Product");
+                });
+
+            modelBuilder.Entity("Aliencube.EntityFrameworkCore.Models.ProductPrice", b =>
+                {
+                    b.Property<int>("ProductPriceId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("ProductPriceId")
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<int>("ProductId")
+                        .HasColumnName("ProductId");
+
+                    b.Property<DateTimeOffset>("ValidFrom")
+                        .HasColumnName("ValidFrom");
+
+                    b.Property<DateTimeOffset?>("ValidTo")
+                        .HasColumnName("ValidTo");
+
+                    b.Property<decimal>("Value")
+                        .HasColumnName("Value");
+
+                    b.HasKey("ProductPriceId");
+
+                    b.HasIndex("ProductId");
+
+                    b.ToTable("ProductPrice");
+                });
+
+            modelBuilder.Entity("Aliencube.EntityFrameworkCore.Models.ProductPrice", b =>
+                {
+                    b.HasOne("Aliencube.EntityFrameworkCore.Models.Product", "Product")
+                        .WithMany("ProductPrices")
+                        .HasForeignKey("ProductId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ samples/Aliencube.EntityFrameworkCore.Models/Migrations/UserDb/20160711030111_AddUser.Designer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Aliencube.EntityFrameworkCore.Models;
+
+namespace Aliencube.EntityFrameworkCore.Models.Migrations.UserDb
+{
+    [DbContext(typeof(UserDbContext))]
+    [Migration("20160711030111_AddUser")]
+    partial class AddUser
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Aliencube.EntityFrameworkCore.Models.User", b =>
+                {
+                    b.Property<int>("UserId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("UserId")
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnName("Name")
+                        .HasAnnotation("MaxLength", 64);
+
+                    b.HasKey("UserId");
+
+                    b.ToTable("User");
+                });
+        }
+    }
+}
